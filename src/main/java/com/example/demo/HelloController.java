ac025af18609581311f191a0510c03937274f0c2
ArinaShyt <Malahowa1917@mail.ru>
2022-06-02 21:30:52 +0300

    first commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import com.example.demo.DB.Interface;
+import com.example.demo.DB.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class HelloController {
+    public PasswordField password;
+    public TextField loginF;
+    @FXML
+    private Label welcomeText;
+    public String login;
+    public static User user;
+
+    public User authorization(ActionEvent actionEvent) {
+        Interface userCRUD = new Interface();
+        if (!loginF.getText().equals("") && !password.getText().equals("")) {
+            try {
+
+                if(loginF.getText().equals(user.getName()) && password.getText().equals(user.getPassword())) {
+                    HelloApplication.setRoot("Nav");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return user;
+    }
+
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
-module com.example.demo8 {
+module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.persistence;
+    requires java.naming;
+    requires java.sql;
+    requires org.hibernate.orm.core;
 
-
-    opens com.example.demo8 to javafx.fxml;
-    exports com.example.demo8;
+    opens com.example.demo to javafx.fxml;
+    opens com.example.demo.DB;
+    exports com.example.demo;
+    exports com.example.demo.DB;
 }
